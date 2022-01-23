062c89babdcf879726273d6a1ce3806c5cc2e523
Leonides Lemus <Leonides.Lemus.10@gmail.com>
2022-01-23 17:18:28 -0500

    working on search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Header.css';
 import ruinLogo from "../assets/header/logo.jpeg";
+import searchIcon from "../assets/header/search.png"
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="logoContainer">
-          <img src={ruinLogo} className="ruinLogo" alt=""/>
-      </div>
+        <div className="logoContainer">
+            <img src={ruinLogo} className="ruinLogo" alt=""/>
+        </div>
+        <div className="searchBar">
+            <div className="searchIconContainer">
+                <img src={searchIcon} />
+            </div>
+            <input className="searchInput" placeholder="Collection, Item or User..."/>
+        </div>
     </div>
   )
 };
