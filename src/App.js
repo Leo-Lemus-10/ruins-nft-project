634bc3cae4f94f65983194d2933e51163d0cefc7
Leonides Lemus <Leonides.Lemus.10@gmail.com>
2022-01-25 01:13:50 -0500

    added get request for opensea data

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
 import { useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [ruinListData, setRuinListData] = useState([])
+  useEffect(() => {
+    const getNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x523853ba2d7b9d945D2Ed4258a0613b3434f0204&order_direction=asc')
+      console.log(openseaData.data.assets)
+    }
+    return getNfts();
+  }, []) 
   return (
     <div className="app">
       <Header/>
