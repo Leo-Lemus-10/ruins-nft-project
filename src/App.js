908c07aef43f3e037bd0c72e04cb206b288b9e0f
Leonides Lemus <Leonides.Lemus.10@gmail.com>
2022-01-24 23:48:48 -0500

    created collection card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <CollectionCard id={0} name={'Blue'} traits={[{'value': 7}]} image = 'https://ipfs.thirdweb.com/ipfs/QmfJdGALQZqfvS8rynBGb1PuJ5byyJGkq4nEoFXqH46xQr/0.jpeg'/>
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import weth from '../assets/weth.png'
+
+const CollectionCard = ({id, name, traits, image}) => {
+  return (
+    <div className="collectonCard">
+        <img src= {image} alt=""/>
+        <div className="details"></div>
+        <div className="name">
+            {name} <div className="id">{id}</div>
+        </div>
+        <div className="priceContainer">
+            <img src={weth} alt=""/>
+            <div className="price">{traits[0]?.value}</div>
+        </div>
+    </div>
+  )
+};
+
+export default CollectionCard;
